6cb5eb6fd3b3cf4b3069428a3c4fab797ced1095
Thiago Carvalho <85368598+thiagoMYdata@users.noreply.github.com>
2023-09-23 15:39:50 -0300

    Add files via upload

---
+++ day 1/main.py
@@ -0,0 +1,5 @@
+city =  input("Well come to the band name generator\nWhat's name of the city you grew up in? ")
+
+pet_name = input("What's your pet's name? ")
+
+print(f'You band could be {city} {pet_name}')
