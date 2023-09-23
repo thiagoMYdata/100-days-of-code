1eae1401d6ec6b2f25da656cff7dda8c1175f4cf
Thiago Carvalho <85368598+thiagoMYdata@users.noreply.github.com>
2023-09-23 15:43:40 -0300

    Update main.py

--- day 2/main.py
+++ day 2/main.py
@@ -3,5 +3,5 @@ bill = float(input('What was the total bill? ')) *100
 many_people = int(input('How many people to split the bill? '))
 tip_porcentage = int(input('What porcentage tip would you like to give? 10, 12 or 15? ')) /100
 
-bill_solver = ((bill + bill * tip_porcentage)/ many_people)/100
+bill_solver = (bill * (1 + tip_porcentage)/ many_people)/100
 print(f"Each person should pay ${round(bill_solver,2)}")
