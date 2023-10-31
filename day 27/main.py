ac3338c45f74eba80a6ff325ebbf6f3d8908a419
Thiago Carvalho <85368598+thiagoMYdata@users.noreply.github.com>
2023-10-30 23:22:19 -0300

    Add files via upload

--- day 27/main.py
+++ day 27/main.py
@@ -3,10 +3,18 @@ from tkinter import ttk
 
 
 root = tk.Tk()
+root.geometry('300x300')
 
 
-ttk.Label(root, text='isso funciona?').pack(expand=True, fill='both')
+# grid settings
+root.columnconfigure(tuple(range(3)), weight=1, uniform='a')
+# root.columnconfigure(3, weight=2, uniform='a')
+root.rowconfigure(tuple(range(3)), weight = 1, uniform='a' )
 
+
+
+
+ttk.Label(root, text='isso funciona?').grid(column=1, row=1, sticky='NEWS', padx=5, pady=5)
 
 
 
