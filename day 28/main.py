8df697adc2718a076f6086289542244f2f60c27e
Thiago Carvalho <85368598+thiagoMYdata@users.noreply.github.com>
2023-11-09 22:56:25 -0300

    Add files via upload

---
+++ day 28/main.py
@@ -0,0 +1,32 @@
+from tkinter import ttk
+import tkinter as tk
+
+# ---------------------------- CONSTANTS ------------------------------- #
+PINK = "#e2979c"
+RED = "#e7305b"
+GREEN = "#9bdeac"
+YELLOW = "#f7f5dd"
+FONT_NAME = "Courier"
+WORK_MIN = 25
+SHORT_BREAK_MIN = 5
+LONG_BREAK_MIN = 20
+
+# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = tk.Tk()
+window.title('Pomodoro')
+window.config(padx = 100, pady = 50, bg = YELLOW)
+
+canvas = tk.Canvas(width = 200, height=223, bg=YELLOW, highlightthickness=0)
+tomato_img = tk.PhotoImage(file = r'100 days of code\day 28\tomato.png')
+canvas.create_image(100,112, image = tomato_img)
+canvas.create_text(100,130, text='00:00', fill = 'white', font = (FONT_NAME, 35, 'bold'))
+canvas.pack()
+
+
+window.mainloop()
