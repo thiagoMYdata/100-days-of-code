dbb763c6d1b844fef87df5ca65714b0ef326255e
Thiago Carvalho <85368598+thiagoMYdata@users.noreply.github.com>
2023-11-16 23:28:21 -0300

    Add files via upload

--- day 28/main.py
+++ day 28/main.py
@@ -20,15 +20,39 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
 window.title('Pomodoro')
-window.config(padx = 100, pady = 50, bg = YELLOW)
+window.config( bg = YELLOW)     #padx = 100, pady = 50,
 
 canvas = tk.Canvas(width = 200, height=223, bg=YELLOW, highlightthickness=0)
 tomato_img = tk.PhotoImage(file = r'100 days of code\day 28\tomato.png')
 canvas.create_image(100,112, image = tomato_img)
 canvas.create_text(100,130, text='00:00', fill = 'white', font = (FONT_NAME, 35, 'bold'))
-canvas.pack()
+
+
+
+timer_label = ttk.Label(window, text='Timer', foreground=GREEN, background=YELLOW ,font = (FONT_NAME, 35, 'bold'))
+
+start_button = ttk.Button(window, text='Start')
+
+reset_button =  ttk.Button(window, text='Reset')
+
+window.columnconfigure(tuple(range(3)), weight=1, uniform='a')
+
+window.rowconfigure(0, weight=4, uniform='a')
+window.rowconfigure(1, weight=8, uniform='a')
+window.rowconfigure(2, weight=1, uniform='a')
+window.rowconfigure(3, weight=1, uniform='a')
+window.rowconfigure(4, weight=2, uniform='a')
+
+
+canvas.grid(row=1, column=1, sticky='NEWS')
+
+timer_label.grid(row=0, column=1, sticky='S')
+
+start_button.grid(row=2, column=0, sticky='E')
+
+reset_button.grid(row=2, column=2, sticky='W')
+
+check_mark = '✅︎'
 
 
 window.mainloop()
-
-gonna work on this file tomorrow
